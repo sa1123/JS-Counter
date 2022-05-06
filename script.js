ea0213637a24d2a815029b4de4aab19c1c4709bb
sa1123 <sahil2398@yahoo.com>
2022-05-07 00:33:07 +0530

    js and more css

--- script.js
+++ script.js
@@ -2,5 +2,44 @@ var countInterval;
 
 function startCounter() {
 
-    var 
+    var number = parseInt(document.getElementById("number").value);
+
+    if (isNaN(number) || number < 1 || number > 9) {
+        alert("Enter a valid number");
+        clearInterval(countInterval);
+        return;
+    }
+
+    var currentNo = document.querySelector(".counter .current");
+    var nextNo = document.querySelector(".counter .next");
+    var count = 0;
+
+    resetNumbers(currentNo, nextNo);
+    clearInterval(countInterval);
+
+    countInterval = setInterval(function () {
+        if (count === number) {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+        increaseCount(currentNo, nextNo);
+        count++;
+    }, 1000);
+}
+
+function resetNumbers(currentNo, nextNo, end) {
+    currentNo.innerText = 0;
+    nextNo.innerText = 1;
+}
+
+function increaseCount(currentNo, nextNo) {
+
+    nextNo.classList.add("animate");
+
+    setTimeout(function () {
+        currentNo.innerText = nextNo.innerText;
+        nextNo.classList.remove("animate");
+        nextNo.innerText = parseInt(nextNo.innerText) + 1;
+    }, 500);
 }
